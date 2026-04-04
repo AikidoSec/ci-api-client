db21b2822eec4baf734986830042a218be677327
Kemosabert <bert@aikido.dev>
2026-04-04 09:55:33 +0200

    bundle all code into one package

--- lib/commands/apiKey.d.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from 'commander';
-declare function cli(apiKey: string): void;
-export declare const cliSetup: (program: Command) => Command;
-declare const _default: {
-    cli: typeof cli;
-    cliSetup: (program: Command) => Command;
-};
-export default _default;
--- lib/configuration.d.ts
+++
@@ -1,4 +0,0 @@
-import Configstore from 'configstore';
-export declare const getConfigStore: () => Configstore;
-export declare const getApiKey: () => any;
-export declare const setApiKey: (newApiKey: string) => void;
